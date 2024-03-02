3889a18ad3c71313c856ac1eb395d5a25d053c0f
dotdotmod <tybaxter2000@gmail.com>
2024-03-02 19:51:55 +0200

    improved test stage and camera system added

---
+++ assets/native/scripts/camera_system.gd
@@ -0,0 +1,35 @@
+extends Spatial
+
+
+var v : float
+var h : float
+
+var camera_mode := "Manual"
+
+export var h_sensitivity := 0.5
+export var v_sensitivity := 0.5
+var camera_lookat_pos = global_translation
+
+onready var cam_pivot = $camera_pivot
+onready var camera = $camera_pivot/Camera
+
+func switch_camera_mode(mode:String):
+	camera_mode = mode
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		v -= event.relative.y * v_sensitivity
+		v = clamp(v, -40, 40)
+		h -= event.relative.x * h_sensitivity
+
+func _process(delta):
+	match camera_mode:
+		"Manual":
+			camera.set_as_toplevel(false)
+			cam_pivot.spring_length = 5
+			cam_pivot.rotation_degrees.y = lerp(cam_pivot.rotation_degrees.y, h, 0.5)
+			cam_pivot.rotation_degrees.x = lerp(cam_pivot.rotation_degrees.x, v, 0.5)
+
--- assets/native/scripts/player.gd
+++ assets/native/scripts/player.gd
@@ -2,12 +2,13 @@ extends KinematicBody
 
 
 var walk_speed := 5.0
-var run_speed := 15.0
+var run_speed := 12.0
 var model_angular_speed := 0.25
 var final_active_speed : float
 var velocity : Vector3
+var gravity_calc : float
 
-onready var camera = $camera_system/SpringArm/Camera
+onready var camera = $camera_system/camera_pivot/Camera
 onready var camera_core = $camera_system
 onready var model_core = $model_core
 
@@ -22,18 +23,29 @@ func _physics_process(delta):
 	raw_input_dir = Vector3(Input.get_action_strength("move_rightways") - Input.get_action_strength("move_leftways"), 0,
 	Input.get_action_strength("move_backwards") - Input.get_action_strength("move_forward"))
 	
-	var final_input = raw_input_dir.rotated(Vector3.UP, camera.rotation.y).normalized()
+	var final_input = raw_input_dir.rotated(Vector3.UP, camera.global_rotation.y).normalized()
 	
+	#Sprinting
 	if Input.is_action_pressed("sprint"):
 		active_speed = final_input.length() + run_speed
 	else:
 		active_speed = final_input.length() + walk_speed
 	
+	#Final active speed
 	final_active_speed = lerp(final_active_speed, active_speed, 0.2)
 	
+	#Setting the velocities
 	base_velocity = final_input * final_active_speed
 	velocity = lerp(velocity, base_velocity, 0.2)
 	
+	if not is_on_floor():
+		gravity_calc -= 20 * delta
+		velocity.y = gravity_calc
+	else:
+		gravity_calc = 0
+		velocity.y = gravity_calc
+	
+	#Move and slide
 	move_and_slide(velocity, Vector3.UP)
 
 
